310654ebfc27a9f27a72ee89908f13cce94641be
GraysonDiehl <138944986+origami183@users.noreply.github.com>
2024-04-09 13:24:17 -0400

    Custom Panel Class (framework)

---
+++ src/main/java/ornify/CustomPanel.java
@@ -0,0 +1,58 @@
+package ornify;
+
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class CustomPanel implements ActionListener
+{
+  public static final int WIDTH = 600;
+  public static final int HEIGHT = 600;
+  
+  public JButton nextButton;
+  public JButton returnButton;
+  public JPanel panel;
+//  public ??? choices; (filler attribute until we decide what the type is)
+  
+  public CustomPanel(String question)
+  {
+    this.panel = new JPanel();
+    this.panel.setLayout(null);
+    this.panel.setBounds(0, 0, WIDTH, HEIGHT);
+    
+    JLabel label = new JLabel(question);
+    label.setFont(new Font("Verdana", Font.BOLD, 30));
+
+    label.setBounds((WIDTH / 2) - 200, 50, 500, 100);
+    
+    this.nextButton = new JButton("Next");
+    this.nextButton.setBounds((WIDTH / 2) - 50, (HEIGHT / 2) + 200, 100, 50);
+    this.nextButton.addActionListener(this);
+    this.returnButton = new JButton("Return");
+    this.returnButton.setBounds((WIDTH / 2) - 50, (HEIGHT / 2) + 100, 100, 50);
+    this.returnButton.addActionListener(this);
+    
+    this.panel.add(label);
+    this.panel.add(this.nextButton);
+    this.panel.add(this.returnButton);
+    this.panel.setVisible(true);
+  }
+  
+  public JPanel getPanel()
+  {
+    return this.panel;
+  }
+  
+  
+
+  @Override
+  public void actionPerformed(ActionEvent e)
+  {
+    BaseApplication baseApplication = new BaseApplication(null);
+    baseApplication.actionPerformed(e);
+  }
+}
