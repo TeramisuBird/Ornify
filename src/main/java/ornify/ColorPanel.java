035e179ff9bcb8756e297120ef5581ca94c0dbfc
GraysonDiehl <138944986+origami183@users.noreply.github.com>
2024-04-11 09:31:02 -0400

    rework for color options based on bird part region

--- src/main/java/ornify/ColorPanel.java
+++ src/main/java/ornify/ColorPanel.java
@@ -2,10 +2,33 @@ package ornify;
 
 public class ColorPanel extends CustomPanel
 {
+  
   private static final String[] COLORS = {"Orange", "Red", "Black", "Brown", "Tan", "Green",
-      "Yellow", "White", "Grey", "Blue"};
-  private static final String[] REGIONS = {"Beak", "Crown", "Supercilium", "Auriculars", "Throat",
-      "Breast", "Wing", "Coverts", "Tail", " Feet"};
+      "Yellow", "White", "Gray", "Blue"};
+  
+  private static final String[] BEAK = {"Orange", "Red", "Black", "Brown", "Tan", "Green",
+      "Yellow", "White", "Gray", "Blue", "Light Tan"};
+  private static final String[] CROWN = {"Orange", "Red", "Black", "Brown", "Tan", "Green",
+      "Yellow", "White", "Gray", "Blue", "Mute Tan", "Iridescent"};
+  private static final String[] SUPERCILIUM = {"Orange", "Red", "Black", "Brown", "Tan", "Green",
+      "Yellow", "White", "Gray", "Blue", "Mute Tan", "Iridescent", "White Tan"};
+  private static final String[] EYESTRIPE = {"Orange", "Red", "Black", "Brown", "Tan", "Green",
+      "Yellow", "White", "Gray", "Blue"};
+  private static final String[] AURICULARS = {"Orange", "Red", "Black", "Brown", "Tan", "Green",
+      "Yellow", "White", "Gray", "Blue", "Mute Tan", "White Tan"};
+  private static final String[] THROAT = {"Orange", "Red", "Black", "Brown", "Tan", "Green",
+      "Yellow", "White", "Gray", "Blue"};
+  private static final String[] BREAST = {"Orange", "Red", "Black", "Brown", "Tan", "Green",
+      "Yellow", "White", "Gray", "Blue", "Tan"};
+  private static final String[] WING = {"Orange", "Red", "Black", "Brown", "Tan", "Green",
+      "Yellow", "White", "Gray", "Blue"};
+  private static final String[] COVERTS = {"Orange", "Red", "Black", "Brown", "Tan", "Green",
+      "Yellow", "White", "Gray", "Blue"};
+  private static final String[] FEET = {"Orange", "Red", "Black", "Brown", "Tan", "Green",
+      "Yellow", "White", "Gray", "Blue"};
+  
+  private static final String[] REGIONS = {"Beak", "Crown", "Supercilium", "Eyestripe", "Auriculars", "Throat",
+      "Breast", "Wing", "Coverts", " Feet"};
 
   public ColorPanel(String question, BaseApplication ba)
   {
