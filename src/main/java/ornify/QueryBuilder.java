fbe32d194ac15b0f7f1473b2ddb2324f8a1d8090
Joseph B <bletheja@dukes.jmu.edu>
2024-04-27 16:39:19 -0400

    Fixed Query Builder errors

--- src/main/java/ornify/QueryBuilder.java
+++ src/main/java/ornify/QueryBuilder.java
@@ -1,42 +1,60 @@
 package ornify;
 
-
 public class QueryBuilder
 {
-  
-  
+
   public QueryBuilder()
   {
 
   }
-  
+
   public String buildQuery()
   {
-    String start = "select name, image_url, sound_url from bird";
-    
-    int selections = 0;
+    String start = "select name, image_url, sound_url from ornify.bird";
+    int emptySelections = 0;
+    boolean firstSelection = true;
+
+    // Checks for zero selections.
+    for (int i = 0; i < Model.selections.length; i++)
+    {
+      if (Model.selections[i] == null || Model.selections[i].isBlank())
+      {
+        emptySelections++;
+      }
+    }
+    if (14 < emptySelections)
+    {
+      return start;
+    }
+
+    // Must have some selection.
     for (String s : Model.selectionIndicies.keySet())
     {
       int index = Model.selectionIndicies.get(s);
-      
+
       if (Model.picked[index])
       {
         String chunk = Model.selections[index];
-        
-        if (selections == 0)
+
+        if (chunk == null || chunk.isBlank())
+        {
+          continue;
+        }
+
+        if (firstSelection)
         {
           chunk = " where " + chunk + "";
+          firstSelection = false;
         }
         else
         {
           chunk = " and " + chunk + "";
         }
-        
+
         start = start + chunk;
-        selections++;
       }
     }
-    
+
     return start;
   }
 }
